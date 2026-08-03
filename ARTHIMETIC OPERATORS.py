eefb26845bd65d217ab5c3caafdd71be4c00eb91
sanjana9119 <sanjanareddy10999@gmail.com>
2026-08-03 13:58:00 +0530

    Add files via upload

---
+++ ARTHIMETIC OPERATORS.py
@@ -0,0 +1,95 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=2
+b=10
+print(a+b)
+12
+a=12
+b=2
+print(a//b)
+6
+a=10
+b=12
+print(b-a)
+2
+a=2
+b=3
+print(a*b)
+6
+print(a**b)
+8
+a+=b
+a
+5
+a-=b
+b
+3
+a*=b
+a
+6
+a%=b
+a
+0
+a>b
+False
+a<b
+True
+a=b
+a==b
+True
+a!=b
+False
+a>=b
+True
+a<=b
+True
+a<b and b>=a
+False
+a<=b and b>=a
+True
+a!=b and a==b
+False
+a<b or b>a
+False
+a>=b or a<=b
+True
+a!=b or a==b
+True
+a=5
+type(a) is int
+True
+type(a) is float
+False
+type(a) is str
+False
+a=1,2,3,4,5,6
+8 in a
+False
+>>> 6 in a
+True
+>>> 10 in a
+False
+>>> 20 not in a
+True
+>>> a=2
+>>> b=4
+>>> a&b
+0
+>>> a=5
+>>> b=7
+>>> a&b
+5
+>>> bin(2)
+'0b10'
+>>> bin(4)
+'0b100'
+>>> bin(7)
+'0b111'
+>>> bin(10)
+'0b1010'
+>>> a=3
+>>> a<<2
+12
+>>> a=5
+>>> a<<3
+40
