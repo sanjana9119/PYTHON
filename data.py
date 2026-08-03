a1e643bea7c89466f24b5a77f61d8123e3e256e9
sanjana9119 <sanjanareddy10999@gmail.com>
2026-08-03 12:38:48 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+'''import MODULES
+MODULES.greet("Sanjana")'''
+'''
+import  MODULES
+'''
+'''
+import  MODULES
+a= MODULES.d["names"]
+b= MODULES.d["id"]
+print(a,b)
+print(b)
+'''
+if __name__=="__main__":
+    a=[10,20,30]
+    a.append("code")
+    a.extend("code")
+    print(a)
+'''
+def dummy():
+    if __name__=="__main__":
+        print("this is script")
+    else:
+        print("this is module")
+dummy()'''
+    
+    
+
