4d07d9118952dc6eccacce3a58fdcf2d7a373029
sanjana9119 <sanjanareddy10999@gmail.com>
2026-08-03 13:15:36 +0530

    Add files via upload

---
+++ VARIABLES.py
@@ -0,0 +1,69 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=10
+print(a)
+10
+a=10;b=20
+print(a+b)
+30
+print(10+3)
+13
+a=2,3,4
+print(a)
+(2, 3, 4)
+a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+a=b=c=10
+print(a,b,c)
+10 10 10
+first_name="Sanjana"
+print(first_name)
+Sanjana
+fname="Sanjana"
+lname="Reddy"
+print(fname+lname)
+SanjanaReddy
+name="Sanjana"
+print(name)
+Sanjana
+#DATATYPES
+Enter "help" below or click "Help" above for more information.
+SyntaxError: invalid syntax. Is this intended to be part of the string?
+A=10
+type(A)
+<class 'int'>
+b=2.0
+type(b)
+<class 'float'>
+c="code"
+>>> type(c)
+<class 'str'>
+>>> c=5+9j
+>>> type(c)
+<class 'complex'>
+>>> b=True
+>>> type(b)
+<class 'bool'>
+>>> #datatype conversations
+>>> #int
+>>> int(10)
+10
+>>> int(8.9)
+8
+>>> int(5)
+5
+>>> float(2.3)
+2.3
+>>> float(3.0)
+3.0
+>>> float(True)
+1.0
+>>> float(False)
+0.0
+>>> bool(5)
+True
+>>> bool(10)
+True
